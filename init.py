1ac37c091d2998aef9692cc966a433b95e73597c
jlew3 <jlew3@users.noreply.github.com>
2018-10-21 15:22:23 -0400

    Add ability to control clouds with gesture
    Uses the average color of a region to detect whether there is a person in a certain region of the cloud. The cloud then uses this information to move away from the person. The cloud can move in left, right, up and down.

--- init.py
+++ init.py
@@ -6,23 +6,31 @@ from pygame.locals import *
 
 cloud_width = 350
 cloud_height = 200
+cloudx = 0
+cloudy = 100
+x = 0
+y = 0
+stepsize = 10
+
+
 cloud = pygame.image.load('cloud.png')
 cloud = pygame.transform.scale(cloud, (cloud_width,cloud_height))
-cloud_right = pygame.Rect(cloud_width/2, 0, cloud_width/2, cloud_height)
-cloud_left = pygame.Rect(0, 0, cloud_width/2, cloud_height)
+cloud_right = pygame.Rect(cloudx + cloud_width/2, cloudy, cloud_width/2, cloud_height)
+cloud_left = pygame.Rect(cloudx, cloudy, cloud_width/2, cloud_height)
+cloud_up = pygame.Rect(cloudx, cloudy, cloud_width, cloud_height/2)
+cloud_down = pygame.Rect(cloudx, cloudy +cloud_height/2, cloud_width, cloud_height/2) 
+threshold = 2  # the change in color required to move the cloud 
 
-
-x = 0 
-y = 0
-step_size = 20
 
 display_width = 1280
 display_height = 720
+
 
 def display_frame(screen, frame):
 	frame = np.rot90(frame)
 	frame = pygame.surfarray.make_surface(frame)
 	screen.blit(frame,(0,0))
+	return frame
 
 def exit(cap):
 	#Close windows
@@ -30,4 +38,29 @@ def exit(cap):
 	pygame.quit()
 	cv.destroyAllWindows()
 
+def move_clouds(x, y):
+	global cloudx, cloudy
+	global cloud_right, cloud_left, cloud_up, cloud_down
+	cloud_right.move_ip(x,y)
+	cloud_left.move_ip(x,y)
+	cloud_up.move_ip(x,y)
+	cloud_down.move_ip(x,y)
+	cloudx += x
+	cloudy += y
 
+def get_brightness(color):
+	return (color[0] + color[1] + color[2])/3
+
+def check_movement(direction, image):
+
+	if direction == "left":
+		subcloud = cloud_left
+	if direction == "right":  
+		subcloud = cloud_right
+	if direction == "up":
+		subcloud = cloud_up
+	if direction == "down":
+		subcloud = cloud_down
+
+	avg_color = pygame.transform.average_color(image, subcloud)
+	return get_brightness(avg_color) > threshold 
--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import sys
 from pygame.locals import *
 from init import *
 
-
-
 #Initialize video feed
 cap = cv.VideoCapture(0)
 
@@ -16,16 +14,18 @@ ret, initial_frame = cap.read()
 pygame.init()
 pygame.display.set_caption("Reach for the Sky")
 screen = pygame.display.set_mode([display_width, display_height])
-#screen.fill([134,173,193])
 
 
 while(1):
-	#Perform background subtraction
 	ret, frame = cap.read( )
 	
 	diff = cv.absdiff(initial_frame, frame)
 	gray = cv.cvtColor(diff, cv.COLOR_BGR2GRAY)
 	(thresh, diff) = cv.threshold(gray, 10, 255 , cv.THRESH_BINARY | cv.THRESH_OTSU)
+
+	screen.fill([0,0,0])
+	image = display_frame(screen, diff)
+	screen.blit(cloud,(cloudx, cloudy))
 
 	for event in pygame.event.get():
 		if event.type == KEYDOWN:
@@ -33,25 +33,35 @@ while(1):
 				ret, initial_frame = cap.read() 
 			else:
 				exit(cap)
-		elif event.type == pygame.MOUSEBUTTONDOWN:
-			pos = pygame.mouse.get_pos()
 
-			if cloud_left.collidepoint(pos):
+	if check_movement("left", image):
+		if cloudx <  display_width - stepsize - cloud_width:  
+			x = stepsize
+	elif check_movement("right", image):
+		if cloudx > stepsize:
+			x = -stepsize
+	if check_movement("up", image):
+		if cloudy < display_height - stepsize - cloud_height:
+			y = stepsize
+	elif check_movement("down", image):
+		if cloudy > stepsize:
+			y = -stepsize
 
-				if x <  display_width - step_size - cloud_width:  
-					cloud_right.move_ip(step_size, 0)
-					cloud_left.move_ip(step_size, 0)
-					x += step_size
-			if cloud_right.collidepoint(pos):
-				if x > step_size:
-					cloud_right.move_ip(-step_size, 0)
-					cloud_left.move_ip(-step_size, 0)
-					x -= step_size
-				
-	screen.fill([0,0,0])
-	display_frame(screen, diff)
-	screen.blit(cloud,(x,y))
+
+
+	if x != 0 or y != 0:
+		cloud_right.move_ip(x,y)
+		cloud_left.move_ip(x,y)
+		cloud_up.move_ip(x,y)
+		cloud_down.move_ip(x,y)
+		cloudx += x
+		cloudy += y
+
+		x = 0
+		y = 0
+
 	pygame.display.update()
 	
 
 exit(cap)
+
