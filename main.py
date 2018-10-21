80180e5d90b5b0619f98d6ce38fa8f4150aa4864
jlew3 <jlew3@users.noreply.github.com>
2018-10-21 11:19:11 -0400

    Change background subtraction method
    Senses people by taking the differenceb between an initial image and the current image. The initial image can be changed by pressing space. Pressing any other key will result in the program closing.

--- main.py
+++ main.py
@@ -1,19 +1,61 @@
 import numpy as np
-import cv2
+import cv2 as cv
+import pygame
+from pygame.locals import *
+import sys
 
-cap = cv2.VideoCapture(0)
+def display_frame(frame):
+	frame = np.rot90(frame)
+	frame = pygame.surfarray.make_surface(frame)
+	screen.blit(frame,(0,0))
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+def exit():
+	#Close windows
+	cap.release()
+	pygame.quit()
+	cv.destroyAllWindows()
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+# Cloud variables 
+cloud = pygame.image.load('cloud.png')
+pygame.transform.scale(cloud, (100,250))
+x = 0 
+y = 0
 
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+#Initialize video feed
+cap = cv.VideoCapture(0)
+fgbg = cv.createBackgroundSubtractorMOG2()
 
-cap.release()
-cv2.destroyAllWindows()
+ret, initial_frame = cap.read()
+
+#Initialize pygame display
+pygame.init()
+pygame.display.set_caption("Reach for the Sky")
+screen = pygame.display.set_mode([1280, 720])
+#screen.fill([134,173,193])
+screen.fill([0,0,0])
+
+while(1):
+	#Perform background subtraction
+	ret, frame = cap.read( )
+	
+	diff = cv.absdiff(initial_frame, frame)
+	#fgmask = fgbg.apply(frame)
+	#cv.imshow('frame',fgmask)
+	gray = cv.cvtColor(diff, cv.COLOR_BGR2GRAY)
+	(thresh, diff) = cv.threshold(gray, 10, 255 , cv.THRESH_BINARY | cv.THRESH_OTSU)
+
+
+	for event in pygame.event.get():
+		if event.type == KEYDOWN:
+			if(event.key) == K_SPACE:
+				ret, initial_frame = cap.read() 
+				x += 1
+			else:
+				exit()
+
+	display_frame(diff)
+	screen.blit(cloud,(x,y))
+	pygame.display.update()
+	
+
+exit()
